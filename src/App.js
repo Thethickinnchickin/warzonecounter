e1f6e4a3853b1c89068f56d2e21d10267b57a739
Matt <matt.reiley@sjsu.edu>
2022-09-26 15:46:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className='row'>
+          <div id="warzone" className='col'>
+            <div className='counterCard'>
+              <div className='cardBody'></div>
+                <div id="card-header">Warzone Victories</div>
+                <div id='body'>
+                  <div class="waviy">
+                    <p>Victories</p>
+                    <span style={{'--i':1}}>2</span>
+                    <span style={{'--i':2}}>1</span>
+  
+
+                  </div>
+                </div>
+                
+            </div>
+          </div>
+          <div id="fallguys" className='col'>
+          <div className='counterCard'>
+              <div className='cardBody'></div>
+                <div id="card-header-fall">Fall Guys Victories</div>
+                <div id='body'>
+                  <div class="waviy">
+                    <p>Victories</p>
+                    <span style={{'--i':1}}>2</span>
+                    <span style={{'--i':2}}>1</span>
+  
+
+                  </div>
+                </div>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
